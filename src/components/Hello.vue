2378de295a6c44ba9147528be108b6c5b7855cc2
Your Name <you@example.com>
2018-04-29 11:17:05 +0200

    axios

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'hello',
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1').then((res) => {
+      console.log(res.data);
+    }).catch(console.log)
+  },
   data () {
     return {
       msg: 'Hello daar'
